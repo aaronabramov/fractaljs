f3284d8435a96a552dc02dca9002b1c0d7bca1f1
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-04-25 23:32:21 -0700

    Async package load

--- async_require.js
+++ async_require.js
@@ -1,11 +1,39 @@
+var MANIFEST = {
+    'package-module-1': 'package_1.js',
+    'package-module-2': 'package_1.js'
+};
+
 (function () {
     var modules = {};
+
+    function loadScript(path, moduleName, callback) {
+        var scriptTag = document.createElement('script');
+
+        scriptTag.src = path;
+
+        scriptTag.onload = function () {
+            callback(requireSync(moduleName));
+        };
+
+        document.body.appendChild(scriptTag);
+    }
 
     function define(moduleName, fn) {
         modules[moduleName] = fn;
     }
 
-    function require() {
+    function require(moduleName, callback) {
+        if (modules[moduleName]) {
+            setTimeout(function () { callback(requireSync(moduleName)); }, 1);
+        } else {
+            for (var m in MANIFEST) {
+                if (moduleName === m) {
+                    loadScript(MANIFEST[m], moduleName, callback);
+                    return;
+                }
+            }
+            throw new Error("Can't find module " + moduleName);
+        }
     }
 
     function requireSync(moduleName) {
