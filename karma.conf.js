1929409f6662fa22866d29571ab9b5910555423a
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-04-26 23:12:16 -0700

    Fix test files path

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
         // list of files / patterns to load in the browser
         files: [
             'test/support/setup.js',
-            'src/async_require.js',
+            'src/assets/async_require.js',
             'test/**/*_test.js'
         ],
 
