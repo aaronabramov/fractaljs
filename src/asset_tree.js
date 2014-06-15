a2eadcfa91edbe4f3debeff6d7ab6532b674f06b
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-06-14 18:23:16 -0700

    s/build/asset_tree/

--- src/asset_tree.js
+++ src/asset_tree.js
@@ -1,29 +1,76 @@
-var AssetNode = require('./asset_node.js');
+var config = require('./config.js'),
+    Q = require('q'),
+    utils = require('./utils.js'),
+    fs = require('fs'),
+    glob = require('glob'),
+    directives = require('./directives.js'),
+    AssetNode = require('./asset_node.js');
 
-function AssetTree() {
-    this.flat = {};
-    this.root = null;
+function makeTree(filePath) {
+    filePath = utils.resolveAssetPath(filePath);
+    // if path has extension, look for exact match, if not then try to match glob
+    if (utils.extractFileType(filePath)) {
+        return getPath(filePath);
+    } else {
+        return getGlob(filePath);
+    }
 }
 
 /**
- * @param assenNode {AssetNode}
- * @return {AssetNode}
+ * @param filePath {String}
+ * @param deferred {Q.deferred} optional default to Q.defer()
+ * @return {Q.promise}
  */
-AssetTree.prototype.setRoot = function(assetNode) {
-    if (!assetNode) { throw new Error('assetNode is not defined'); }
-    return (this.root = this.flat[assetNode.path] = assetNode);
-};
+function getPath(filePath, deferred) {
+    deferred = (deferred || Q.defer());
+    fs.readFile(filePath, function(err, data) {
+        if (err) {
+            deferred.reject(err);
+        } else {
+            recurMakeTree(filePath, data, deferred);
+        }
+    });
+    return deferred.promise;
+}
 
 /**
- * @return {AssetNode}
+ * @param filePath {String} path
+ * @return {Q.promise}
  */
-AssetTree.prototype.append = function(options, parentPath) {
-    var node = this.flat[options.path] || (this.flat[options.path] = new AssetNode(options));
-    parentNode = this.flat[parentPath];
-    if (!parentNode && !this.root) { return this.setRoot(node); }
-    if (!parentNode) { throw new Error('parent node is not found'); }
-    this.flat[parentPath].children.push(node);
-    return node;
-};
+function getGlob(filePath) {
+    var pattern = filePath + '.*';
+    glob(pattern, {
+        cwd: config.assetPath
+    }, function(err, files) {
+        if (err) return deferred.reject(err);
+        if (!files.length) {
+            return deferred.reject('no files found. path: ' + pattern);
+        }
+        getPath(files[0], deferred);
+    });
+    return deferred.promise;
+}
 
-module.exports = AssetTree;
+function recurMakeTree(filePath, data, deferred) {
+    var extractedDirectives = directives.extract(data),
+        filesToRequire = directives.extractFilesToRequire(extractedDirectives),
+        branches = filesToRequire.map(function(path) {
+            return makeTree(path);
+        });
+
+    Q.all(branches).then(function(assetNodes) {
+        // Recursion base case. when there are no files to require it
+        // resolves itself with `children: []` and tree starts to fill
+        // itself from bottom to top.
+        deferred.resolve(new AssetNode({
+            path: filePath,
+            content: data,
+            children: assetNodes,
+            directives: extractedDirectives
+        }));
+    }).fail(function(err) {
+        deferred.reject(err);
+    });
+}
+
+module.exports.makeTree = makeTree;
--- test/asset_tree_test.js
+++ test/asset_tree_test.js
@@ -1,42 +1,31 @@
-var expect = require('chai').expect,
-    AssetTree = require('../src/asset_tree.js'),
-    AssetNode = require('../src/asset_node.js');
+var assetTree = require('../src/asset_tree.js'),
+    config = require('../src/config.js'),
+    path = require('path'),
+    expect = require('chai').expect;
 
 describe('asset_tree.js', function() {
     beforeEach(function() {
-        this.subject = new AssetTree();
+        this._assetPath = config.assetPath;
+        config.assetPath = path.resolve(__dirname, './fixtures');
     });
 
-    describe('constructor', function() {
-        it('initializes flat structure', function() {
-            this.subject.flat.should.be.an('object');
-        });
+    afterEach(function() {
+        config.assetPath = this._assetPath;
     });
 
-    describe('#setRoot', function() {
-        it('sets root node', function() {
-            var node = new AssetNode({
-                path: './test.js',
-                content: ''
+    describe('#makeTree', function() {
+        it('requires submodules', function(done) {
+            assetTree.makeTree('./module1.js').then(function(assetNode) {
+                try {
+                    expect(config.LIB_PATH).to.be.a('string');
+                    expect(assetNode.children.length).to.equal(2);
+                    expect(assetNode.children[0].path).to.equal(config.LIB_PATH);
+                    expect(assetNode.children[1].path).to.equal(path.resolve(config.assetPath, './module2.js'));
+                    done();
+                } catch (e) {
+                    done(e);
+                }
             });
-            this.subject.setRoot(node);
-            expect(this.subject.root).to.an.instanceOf(AssetNode);
-            this.subject.root.path.should.equal('./test.js');
-        });
-    });
-
-    describe('#append', function() {
-        it("sets root node if root is empty and append called without parentPath", function() {
-            this.subject.append({path: './test.js', content: ''});
-            Object.keys(this.subject.flat).length.should.equal(1);
-            expect(this.subject.root).to.be.an.instanceof(AssetNode);
-        });
-
-        it("creates node and appends to it's parent looked up by path", function() {
-            this.subject.append({path: './test.js', content: ''});
-            this.subject.append({path: './sub.js', content: ''}, './test.js');
-            Object.keys(this.subject.flat).length.should.equal(2);
-            this.subject.root.children.length.should.equal(1);
         });
     });
 });
