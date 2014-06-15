7145f8d5555ddeef06ac21b7ecfaaa8c49f14aa0
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-06-14 23:30:04 -0700

    remove redundant code

--- src/directives.js
+++ src/directives.js
@@ -60,34 +60,4 @@ function extract(content) {
     return fileNames;
 }
 
-
-/**
- * @param directives {Array} [['require', 'main.js'], ['require_lib']]
- * @return {Array} of objects [{wrap: true, path: './path.js'}]
- */
-function extractFilesToRequire(directives) {
-    var filePaths = [];
-    directives.forEach(function(d) {
-        filePaths = filePaths.concat(directiveToFiles(d));
-    });
-    return filePaths;
-}
-
-/**
- * Extract filenames that need to be required from directive
- * @param d {Array} [directiveType, directiveValue]
- * @return {Array} of paths
- */
-function directiveToFiles(d) {
-    var type = d[0];
-    switch (d[0]) {
-        case 'require':
-            return [d[1]];
-        case 'require_lib':
-            return [config.LIB_PATH];
-        default:
-            return [];
-    }
-}
-
 module.exports = Directives;
