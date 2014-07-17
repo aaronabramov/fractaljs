729739c9e3658739b34b46045f7a6ae842bac5ac
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-07-16 20:49:02 -0700

    require#loadReferences

--- src/assets/async_require.js
+++ src/assets/async_require.js
@@ -1,7 +1,8 @@
-(function () {
+(function() {
     // Cache variables
-    var modules = {},
-        scriptTags = [];
+    var modules = {}, // cache of modules
+        scriptTags = [],
+        references = {}; // cache of reference maps
 
     /**
      * Load script from the path and bind callback on onload event passing
@@ -16,7 +17,9 @@
         scriptTag.src = path;
         // cache script tag for later cleanup
         scriptTags.push(scriptTag);
-        scriptTag.onload = function () { callback(require(moduleName)); };
+        scriptTag.onload = function() {
+            callback(require(moduleName));
+        };
         document.body.appendChild(scriptTag);
     }
 
@@ -40,9 +43,11 @@
     function require(moduleName) {
         // Init empty exports object and pass it into defining module function
         var exports = {},
-            module = {exports: exports};
-        if (!modules[moduleName]) throw new Error ('module [' + moduleName +
-                                                   '] not found');
+            module = {
+                exports: exports
+            };
+        if (!modules[moduleName]) throw new Error('module [' + moduleName +
+            '] not found');
         modules[moduleName](exports, module);
         return module.exports;
     }
@@ -51,25 +56,28 @@
      * Try to lookup module in cache, if module has been already loaded
      *      return populated exports object via require function, if
      *      module is not loaded but present in MANIFEST list then initialize
-     *      async loading of the scrypt containing this module.
+     *      async loading of the script containing this module.
      *
      * @param moduleName {String} module name
-     * @param callback {Function} function that will be invokod after module
+     * @param callback {Function} function that will be invoked after module
      *      has been fetched and loaded. module.exports will be passed into
      *      callback function.
-     * @throws Will throw an error if module is not foind in cache or package
+     * @throws Will throw an error if module is not found in cache or package
      *      files.
      */
     function use(moduleName, callback) {
         if (modules[moduleName]) {
             // If module is already loaded then defer callback invokation
-            setTimeout(function () { callback(require(moduleName)); }, 1);
+            setTimeout(function() {
+                callback(require(moduleName));
+            }, 1);
         } else {
-            // else try to find module name in the manifest
-            for (var m in MANIFEST) {
+            // else try to find module name in the references
+            for (var m in references) {
                 if (moduleName === m) {
                     // if it's there initialize loading script file
-                    loadScript(MANIFEST[m], moduleName, callback);
+                    var bundlePath = '/assets/' + references[m][0] + '?bundle=true';
+                    loadScript(bundlePath, moduleName, callback);
                     return;
                 }
             }
@@ -78,8 +86,27 @@
         }
     }
 
+    /**
+     * register references
+     *
+     * @param refs {Object} {"module1.js": ["bundle1.js", "bundle2.js"]}
+     */
+    function loadReferences(refs) {
+        for (var module in refs) {
+            if (references[module]) {
+                for (var i = 0, _length = refs[module].length; i < _length; i++) {
+                    references[module].push(refs[module][i]);
+                }
+            } else {
+                references[module] = refs[module];
+            }
+        }
+    }
+
     define.modules = modules;
     define.scriptTags = scriptTags;
+    require.loadReferences = loadReferences;
+    require.references = references;
 
     window.define = define;
     window.require = require;
